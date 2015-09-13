60044a00d87eec96785fe247892628e27671d15a
Linnea <lappl483@newschool.edu>
2015-09-13 19:18:14 +0000

    Added assignment2.js, cleaned up

--- assignment2.js
+++ assignment2.js
@@ -5,26 +5,6 @@ var fileContent = fs.readFileSync('/home/ubuntu/workspace/data/aameetinglist02M.
 
 var $ = cheerio.load(fileContent);
 
-// // declare streetAddress variable globally
-// var streetAddress = ("");
-
-// // use .attr method to target the specific table
-// $('table').attr('cellpadding', '5') {
-//     // target the h4 tags in the table
-//     $('h4').each(function(i, elem){
-//         // each street address is the next, next element after each h4
-//         var meetingName = $(elem).next().each(function(i, elem2) {
-//             var streetAddress = $(elem2).next().each(function(i, elem3) {
-//               return ($(elem3).text());
-//           });
-//         });
-//     });
-// }
-
-// // print the trimmed street address
-// console.log(streetAddress.trim());
-
-
 $('table').each(function(i, elem){
     if ($(elem).attr("cellpadding") == '5') {
        var location = $(elem).find('td:first-child').children().remove().end().text().split("<br />");
@@ -35,7 +15,3 @@ $('table').each(function(i, elem){
           }
     }
 );
-
-
-//continue traversing the table down to the <td> elements that contain the address data
-            //console.log the addresses here for EACH <td> that you have selected in the line above
