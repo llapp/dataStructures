62cdc1e88b60a6b0658709712230749d72794176
Linnea <lappl483@newschool.edu>
2015-09-15 23:45:35 +0000

    Pushing meeting locations into a array

--- wassign02solution.js
+++ wassign02solution.js
@@ -5,10 +5,14 @@ var fileContent = fs.readFileSync('/home/ubuntu/workspace/data/aameetinglist02M.
 
 var $ = cheerio.load(fileContent);
 
+var meetings = [];
+
 $('table[cellpadding=5]').find('tbody').find('tr').each(function(i, elem) {
-    console.log($(elem).find('td').eq(0).html().split('<br>')[2].trim());
+    meetings.push($(elem).find('td').eq(0).html().split('<br>')[2].trim());
     
 });
+
+console.log(meetings);
 
 // .eq(i) use to select matched element at a specified index 
 // in this case we want the third element
