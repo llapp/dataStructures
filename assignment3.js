5c6a0a7f09de65958c13c11df14678f5a1e9e192
Linnea <lappl483@newschool.edu>
2015-09-25 17:49:06 +0000

    Added revision of assignment3.js, error with 'geometry'

--- assignment3.js
+++ assignment3.js
@@ -1,23 +1,34 @@
+var fs  = require("fs");
 var request = require('request'); // npm install request
 var async = require('async'); // npm install async
-var fs  = require("fs");
 
 var apiKey = process.env.API_KEY;
 
 var meetingsData = [];
-var addresses = [fs.readFileSync('/home/ubuntu/workspace/data/addresses.txt').toString().split('\n')];
+var addresses = fs.readFileSync('/home/ubuntu/workspace/data/addresses.txt').toString();
+
+// console.log(addresses);
 
 // eachSeries in the async module iterates over an array and operates on each item in the array in series
 async.eachSeries(addresses, function(value, callback) {
-    var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + value.split(' ').join('+') + '&key=' + apiKey;
+    var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + addresses + '&key=' + apiKey;
     var thisMeeting = new Object;
     thisMeeting.address = value;
     request(apiRequest, function(err, resp, body) {
         if (err) {throw err;}
+        if(body.status === "ZERO_RESULTS") {
+            console.log("ZERO RESULTS for" + thisMeeting.address);
+        } else {
         thisMeeting.latLong = JSON.parse(body).results[0].geometry.location;
         meetingsData.push(thisMeeting);
+        }
     });
-    setTimeout(callback, 2000);
+    setTimeout(callback, 300);
 }, function() {
-    console.log(meetingsData);
+    fs.writeFile('assignment3_data.txt', meetingsData, function (err) {
+        if (err) 
+        return console.log('Error');
+        console.log('Wrote ' + meetingsData.length + 'entries to file ' + 'assignment3_data.txt');
+        
+    });
 });
