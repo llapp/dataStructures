ec1dbbc50f647e98a73f55c8b8eddbf68d6d2bc4
Linnea <lappl483@newschool.edu>
2015-10-04 17:48:22 +0000

    Assignment4.js cleaned up & commented

--- assignment4.js
+++ assignment4.js
@@ -1,14 +1,16 @@
-// IN THE MONGO SHELL: 
-//   CREATE DATABASE aa AND SWITCH TO IT WITH: 
+// ---------------------------------
+// weekly assignment 4 - Linnea Lapp
+// 10-05-2015
+// ---------------------------------
+
+// Commands in the MongoDB shell: 
 //      use aa
-//   CREATE COLLECTION meetings_area2 WITH: 
 //      db.createCollection('meetings_area2')
 
-var request = require('request');
-var fs = require('fs');
+var request = require('request'); // npm install request
+var fs = require('fs'); // npm install fs
 
     // Connection URL
-    // Give the URL for the mongo database
     var url = 'mongodb://localhost:27017/aa';
    
     var meetingInfo = JSON.parse(fs.readFileSync('/home/ubuntu/workspace/data/assignment3_data.txt'));
@@ -22,13 +24,11 @@ var fs = require('fs');
 
         var collection = db.collection('meetings_area2');
 
-        // THIS IS WHERE THE DOCUMENT(S) IS/ARE INSERTED TO MONGO:
-        // inserting each i from the stationBeanList array into the database
-        // can insert just an array of objects without using a for loop
+        // Insert info from meetingInfo to the documents collection 'meetings_area2'
         for (var i=0; i < meetingInfo.length; i++) {
             collection.insert(meetingInfo[i]);
             console.log("Inserted " + meetingInfo.length +" documents into the document collection");
             }
         db.close();
 
-    }); //MongoClient.connect
+    });
