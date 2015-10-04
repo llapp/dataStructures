08db67cb394b3735e65622a6f58ac1cea9cdb18a
Linnea <lappl483@newschool.edu>
2015-10-04 18:22:03 +0000

    Working file, saves all 28 locations

--- assignment4.js
+++ assignment4.js
@@ -10,25 +10,41 @@
 var request = require('request'); // npm install request
 var fs = require('fs'); // npm install fs
 
-    // Connection URL
-    var url = 'mongodb://localhost:27017/aa';
-   
-    var meetingInfo = JSON.parse(fs.readFileSync('/home/ubuntu/workspace/data/assignment3_data.txt'));
+// Connection URL
+var url = 'mongodb://localhost:27017/aa';
 
-    // Retrieve
-    var MongoClient = require('mongodb').MongoClient; // npm install mongodb
+var meetingInfo = JSON.parse(fs.readFileSync('/home/ubuntu/workspace/data/assignment3_data.txt'));
+
+var MongoClient = require('mongodb').MongoClient
+  , assert = require('assert');
+ 
+// Use connect method to connect to the Server 
+MongoClient.connect(url, function(err, db) {
     
-    // db = connection to the databas (if there is one)
-    MongoClient.connect(url, function(err, db) {
-        if (err) {return console.dir(err);}
+  // if there was an error, print error
+  assert.equal(null, err);
+  
+  // confirm successful connection to server
+  console.log("Connected correctly to server");
+ 
+  insertDocuments(db, function() {
+    
+    // close connection
+    db.close();
+  });
+});
 
-        var collection = db.collection('meetings_area2');
-
-        // Insert info from meetingInfo to the documents collection 'meetings_area2'
-        for (var i=0; i < meetingInfo.length; i++) {
-            collection.insert(meetingInfo[i]);
-            console.log("Inserted " + meetingInfo.length +" documents into the document collection");
-            }
-        db.close();
-
-    });
+// function to insert info into documents, and documents into collection
+var insertDocuments = function(db, callback) {
+  // get the documents collection 
+  var collection = db.collection('meetings_area2');
+  // insert info from meetingsInfo to document collection
+  collection.insert(
+    meetingInfo, function(err, result) {
+    assert.equal(err, null);
+    assert.equal(meetingInfo.length, result.result.n);
+    assert.equal(meetingInfo.length, result.ops.length);
+    console.log("Inserted " + meetingInfo.length +" documents into the document collection");
+    callback(result);
+  });
+};
