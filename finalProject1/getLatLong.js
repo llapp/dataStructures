11fe3bf90135a3f4e2f53166751d182f854fea60
Linnea <lappl483@newschool.edu>
2015-12-04 02:54:11 +0000

    Successfully wrote all lat/longs to txt file

--- finalProject1/getLatLong.js
+++ finalProject1/getLatLong.js
@@ -9,9 +9,9 @@ var async = require('async');
 var fs = require('fs');
 var cheerio = require('cheerio'); // npm install cheerio
 
-var apiKey = process.env.API_KEY;
+var apiKey = 'AIzaSyD9LGffe6_uRk8GthCYa05bXaRejYsWQao'; //process.env.API_KEY;
 var apiAddress = [];
-var latLong = [];
+var meetingsData = [];
 
 var content = fs.readFileSync('/home/ubuntu/workspace/data/finalProject1/allMeetingInfo.txt');
 
@@ -23,38 +23,28 @@ $('table[cellpadding=5]').find('tbody').find('tr').each(function(i, elem) {
     apiAddress.push($(elem).find('td').eq(0).html().split('<br>')[2].trim());
 });
     // console.log(apiAddress);
-
+    
 async.eachSeries(apiAddress, function(value, callback) {
-    var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + fixAddresses(value).split(' ').join('+') + '&key=' + apiKey;
+    var mymyAddress = fixAddresses(value).split(' ').join('+');
+    console.log(mymyAddress);
+    var apiRequest = 'https://maps.googleapis.com/maps/api/geocode/json?address=' + mymyAddress + '&key=' + apiKey;
     var thisMeeting = new Object;
-    thisMeeting.address = value;
-
+    // thisMeeting.address = value;
     request(apiRequest, function(err, resp, body) {
-        if (err) {
-            throw err;
-        }
-        
-        if (JSON.parse(body).status == "ZERO_RESULTS") {
-            console.log("ZERO RESULTS for" + value);
-        } else {
-            
-            thisMeeting.latLong = JSON.parse(body).results[0].geometry.location;
-            console.log(latLong.length);
-        }
-
-        // latLong.push(allInfo.meetingLatLong);
-
-    setTimeout(callback, 300);
-        
-    }, function() {
-    fs.writeFile('meetingLatLong.txt', JSON.stringify(latLong), function (err) {
+        if (err) {throw err;}
+        thisMeeting.latLong = JSON.parse(body).results[0].geometry.location;
+        meetingsData.push(thisMeeting);
+    });
+    setTimeout(callback, 2000);
+}, function() {
+            // console.log(meetingLatLongs);
+    fs.writeFile('allLatLongs.txt', JSON.stringify(meetingsData), function (err) { 
         if (err) 
         return console.log('Error');
-        console.log('Wrote ' + latLong.length + ' entries to file ' + 'meetingLatLong.txt');
+        console.log('Wrote ' + meetingsData.length + ' entries to file');
         
     });
-});
-});
+});  
 
 function fixAddresses(oldAddress) {
     // want to get rid of anything in () before the comma
